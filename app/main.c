cfa8d1cd17aae4cb9f5ec509b32002caff4a837d
Jaakko <rantamaki@live.fi>
2020-10-17 14:11:23 +0300

    Progress

--- app/main.c
+++ app/main.c
@@ -6,6 +6,7 @@
 Muuttujien esittely*/
 int pinOk = 0 ;
 int userAccount = 0;
+int userAccountPin = 2345;
 int userAccountPinInput = 0 ;
 int checksum = 0 ;
 int checksumInput = 0 ;
@@ -14,7 +15,25 @@ int sessionOn = 0 ;
 /*
 Main funktio ja sen esittely näissä kommenteissa*/
 int main() {
+/*
+    Kysytään pin koodia niin kauan että tärppää.
+*/
+    while (pinOk == 0) {
     printf("Syötä käyttäjätunnus \n");
-    scanf("%d", userAccount);
+    scanf("%i", &userAccount);
+    printf("Anna tunnusluku \n");
+    scanf("%d", &userAccountPinInput);
+
+        if (userAccountPinInput == userAccountPin) {
+            pinOk = 1;
+            sessionOn = 1;
+        }
+            else{
+                printf("PIN -väärin \n");
+        }
+    }
+    while (sessionOn == 1 ){
+        printf("MOIKKIS");
+    }
     return (0);
 }
