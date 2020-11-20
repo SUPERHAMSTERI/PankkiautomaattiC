c1bcd2416620852fc92075fcceb48c4d77be50db
Jaakko <rantamaki@live.fi>
2020-11-20 20:49:18 +0200

    Palautusehdokas 2, Siivousta. Lilla buggenfixen

--- main.c
+++ main.c
@@ -177,8 +177,8 @@ int banknote (){
     int withdrawalSumN;             // Käytetään laskemaan seteleitä, jos summa ei ole jaollinen 20:llä tai 50llä.
     int withdrawalSumY;             // Käytetään laskemaan seteleitä, jos summa ei ole jaollinen 20:llä tai 50llä.
 
-    if(withdrawalSum == 30 || 1000 < withdrawalSum || withdrawalSum < 20 || withdrawalSum % 10){
-        printf("Et voi nostaa %d EUR. Tarjoamme vain 20 tai 40 - 1000 EUR suuruisia nostoja." "\n", withdrawalSum);
+    if(withdrawalSum == 30 || 1000 < withdrawalSum || withdrawalSum < 20 || withdrawalSum % 10 != 0){
+        printf("Et voi nostaa %d EUR. Tarjoamme vain 20 EUR tai 40 - 1000 EUR suuruisia nostoja." "\n", withdrawalSum);
         printf("Voit valita summan kymmenen euron portaittain." "\n");
         printf("Valitse toinen mieluisa summa tarjoamistamme vaihtoehdoista!" "\n");
         inLoop = 1;
